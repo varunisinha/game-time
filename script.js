f6b69528b5f06690df95380589fd0c5ea7b5ff22
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-07-19 07:24:15 -0400

    When city selected, table will display dates and weather icons in each row

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const API_KEY = "0773ca9aaf5c56e841a981221d072a10";
 var tableEl = document.querySelector("table");
 var formEl = document.querySelector("#city-date-form");
 var cityEl = document.querySelector("#city-dropdown");
+var city;
 
 // Create header row
 var rowEl = document.createElement("tr");
@@ -17,19 +19,26 @@ for(var i=0; i<7; i++){
     rowEl = document.createElement("tr");
     for(var j=0; j<3; j++){
         var tableDataEl = document.createElement("td");
-        tableDataEl.innerHTML = headers[j];
+        var dateEl = document.createElement("p");
+        var weatherIconEl = document.createElement("img");
+        
+        tableDataEl.appendChild(dateEl);
+        tableDataEl.appendChild(weatherIconEl);
         rowEl.appendChild(tableDataEl);
+        // set ids for first column
+        if(j===0){
+            tableDataEl.setAttribute("id", "day" + i);
+            dateEl.setAttribute("id", "date" + i);
+            weatherIconEl.setAttribute("id", "weather" + i);
+        }
     }
     tableEl.appendChild(rowEl);
 }
 
 // Fetch weather data
-const API_KEY = "0773ca9aaf5c56e841a981221d072a10";
-
-// access data-city for each drop down menu item and use that information to read in api weather
-var city;
 formEl.addEventListener("submit", formSubmit);
 
+// access data-city for each drop down menu item and use that information to read in api weather
 function formSubmit(event){
     event.preventDefault();
     getLatLon(cityEl.value);
@@ -56,7 +65,7 @@ function getLatLon(city){
         })
 }
 
-// Get weather based on lat and lon
+// Get and display weather based on lat and lon
 function getWeather(url){
     fetch(url)
         .then(function (response) {
@@ -64,5 +73,32 @@ function getWeather(url){
         })
         .then(function (data) {
             console.log(data);
+
+            for (var i=0; i<7; i++){
+                var dateId = "#date" + i;
+                var weatherId = "#weather" + i;
+                var dayEl = document.querySelector(dateId);
+                var weatherEl = document.querySelector(weatherId);
+                var icon = "https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png";
+
+                dailyDate = getDate(data.daily[i].dt);
+                dayEl.innerHTML = dailyDate.shortWeekday + ", " + dailyDate.monthNum + "/" + dailyDate.day;
+                weatherEl.setAttribute("src", icon);
+            }
         })
+}
+
+// Read in unix date and return standard date
+function getDate(unix) {
+    var timeStamp = unix * 1000;
+    var date = new Date(timeStamp);
+
+    var dateObj = {
+        shortWeekday: date.toLocaleString("en-US", { weekday: "short" }), 
+        longWeekday: date.toLocaleString("en-US", { weekday: "long" }),
+        monthName: date.toLocaleString("en-US", {month: "long"}), 
+        monthNum: date.toLocaleString("en-US", { month: "numeric" }), 
+        day: date.toLocaleString("en-US", { day: "numeric" })
+    };
+    return dateObj;
 }
